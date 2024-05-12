23b9b0c5325df8fb6c5033e3ecd80447d5f0f232
DrRawley <rawley.greene@gmail.com>
2024-05-12 15:13:36 -0400

    edit settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,30 +12,33 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv.load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^dq=a3j5+27*2&vex)x8v!&o-l$n*j+m(vit16$_pzjs!g#1id'
-#SECRET_KEY = os.getenv("SECRET_KEY")
+#SECRET_KEY = 'django-insecure-^dq=a3j5+27*2&vex)x8v!&o-l$n*j+m(vit16$_pzjs!g#1id'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.replit.dev']
+ALLOWED_HOSTS = ['drrawley.pythonanywhere.com', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://*.replit.dev',
-    'https://64582965-f536-4bc5-b3a2-105a25221cee-00-anpmqpcpyxmt.spock.replit.dev/'
+    'https://drrawley.pythonanywhere.com',
+    'https://127.0.0.1',
+    'http://127.0.0.1'
 ]
 
 # This allows django to display sites under replit
-X_FRAME_OPTIONS = '*'
+# X_FRAME_OPTIONS = '*'
 
 # Application definition
 
@@ -126,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
