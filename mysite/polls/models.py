0e92f512c866b9dacba135440714ee53ae573a11
DrRawley <rawley.greene@gmail.com>
2024-05-11 22:41:32 -0400

    implemented testing

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -11,9 +11,9 @@ class Question(models.Model):
     return self.question_text
   
   def was_published_recently(self):
-    return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now #fix error returning true for future publications
 
-  pass
 
 class Choice(models.Model):
   question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -23,5 +23,4 @@ class Choice(models.Model):
   def __str__(self):
     return self.choice_text
 
-  pass
 
